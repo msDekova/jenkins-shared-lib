0c20d9eafa30418bab4f64995445a41d3a86c2a8
Maria Dekova <62178057+msDekova@users.noreply.github.com>
2022-06-08 11:08:07 +0300

    Update bytesInKBytes.groovy

--- vars/bytesInKBytes.groovy
+++ vars/bytesInKBytes.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(o_used, o_total, a_used, a_total) {
-//    def o_used = 1193555
-//    def o_total = 1193814
-//    def a_used = 120931
-//    def a_total = 122132
     def o_used_kb = o_used.div(1024)
     def o_total_kb = o_total.div(1024)
     def o_used_total = o_used_kb*100.div(o_total_kb)
@@ -13,8 +9,8 @@ def call(o_used, o_total, a_used, a_total) {
     def a_total_kb = a_total.div(1024)
     def a_used_total = a_used_kb*100.div(a_total_kb)
 
-    println ('O: used in kB ' + o_used_kb + ' total in kB ' + o_total_kb  + ' used of total ' + o_used_total)
-    println ('A: used in kB ' + a_used_kb  + ' total in kB ' + a_total_kb + ' used of total ' + a_used_total)
+    println ('O: used in kB ' + o_used_kb.round(2) + ' total in kB ' + o_total_kb.round(2) + ' used of total ' + o_used_total.round(2) + '%')
+    println ('A: used in kB ' + a_used_kb.round(2) + ' total in kB ' + a_total_kb.round(2) + ' used of total ' + a_used_total.round(2) + '%')
 }
 
 return this;
