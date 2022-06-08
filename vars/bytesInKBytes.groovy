598e6e2a241cc337225a1f7463dff14c4ef2adfd
Maria Dekova <62178057+msDekova@users.noreply.github.com>
2022-06-08 11:00:44 +0300

    Update bytesInKBytes.groovy

--- vars/bytesInKBytes.groovy
+++ vars/bytesInKBytes.groovy
@@ -11,7 +11,7 @@ def call(o_used, o_total, a_used, a_total) {
     
     def a_used_kb = a_used.div(1024)
     def a_total_kb = a_total.div(1024)
-    def a_used_total = a_used_b*100.div(a_total_b)
+    def a_used_total = a_used_kb*100.div(a_total_kb)
 
     println ('O: used in kB ' + o_used_kb + ' total in kB ' + o_total_kb  + ' used of total ' + o_used_total)
     println ('A: used in kB ' + a_used_kb  + ' total in kB ' + a_total_kb + ' used of total ' + a_used_total)
